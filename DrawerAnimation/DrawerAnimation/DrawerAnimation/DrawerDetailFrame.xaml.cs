4f188907e88048904ea0caf8a564a3ca896116bc
Samet <samet.sigirci@tulpar.io>
2019-12-10 12:20:21 +0300

    new drawer

---
+++ DrawerAnimation/DrawerAnimation/DrawerAnimation/DrawerDetailFrame.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DrawerAnimation
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DrawerDetailFrame : ContentView
+    {
+        bool isExpanded = false;
+        public DrawerDetailFrame()
+        {
+            InitializeComponent();
+        }
+
+        private void Frame_Tapped(object sender, EventArgs e)
+        {
+            if (!isExpanded)
+                AnimateIn();
+            else
+                AnimateOut();
+
+            isExpanded = !isExpanded;
+        }
+
+        private void AnimateIn()
+        {
+            Dog1Frame.LayoutTo(new Rectangle(Dog1Frame.X, Dog1Frame.Y, Dog1Frame.Width, 275), 500, Easing.SinOut);
+            SelectedValue = 275;
+        }
+
+        private void AnimateOut()
+        {
+            Dog1Frame.LayoutTo(new Rectangle(Dog1Frame.X, Dog1Frame.Y, Dog1Frame.Width, 75), 500, Easing.SinOut);
+            SelectedValue = 75;
+        }
+
+        public static readonly BindableProperty SelectedValueProperty =
+BindableProperty.Create("SelectedValue", typeof(double), typeof(DrawerDetailFrame), defaultBindingMode: BindingMode.OneWay);
+        public double SelectedValue
+        {
+            get { return ((double)(base.GetValue(SelectedValueProperty))); }
+            set { SetValue(SelectedValueProperty, value); }
+        }
+    }
+}
---
+++ DrawerAnimation/DrawerAnimation/DrawerAnimation/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DrawerAnimation
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        double SelectedVal = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Frame1_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (((DrawerDetailFrame)sender).SelectedValue != SelectedVal && Frame2.IsVisible)
+            {
+                SelectedVal = ((DrawerDetailFrame)sender).SelectedValue;
+                Frame2.TranslateTo(Frame2.TranslationX, SelectedVal == 275 ? Frame2.TranslationY + 200 : Frame2.TranslationY - 200, 500, Easing.SinOut);
+            }
+        }
+    }
+}
